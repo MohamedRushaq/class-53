24b06f604b300a50f39f98959f1aed8a9c353e7c
MohamedRushaq <rinazraleef@gmail.com>
2025-05-07 21:25:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# program to show constant time complexity
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration+=1
+    print("Total iteration done by the code is" ,iteration,"\n")
+
+
+
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' the time taken by our code won't change")    
+
---
+++ main2.py
@@ -0,0 +1,12 @@
+def onTime(n):
+    iteration=0
+    for 1 in range(1,n+1):
+       iteration+=1
+    print("when n is ",n,"iteration = ", iteration)
+
+
+onTime(10)
+onTime(20)
+onTime(42)       
+
+print("\n with every 'n' the time taken and iteration will increase")
